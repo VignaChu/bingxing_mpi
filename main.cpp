b891f26101b43ad45998cbd29c65a4e3a7c4ae20
VignaChu <1113820920@qq.com>
2025-06-09 23:41:19 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,93 @@
+#include "PCFG.h"
+#include <chrono>
+#include <fstream>
+#include "md5.h"
+#include <iomanip>
+#include <mpi.h>
+using namespace std;
+using namespace chrono;
+
+// 编译指令如下
+// g++ main.cpp train.cpp guessing.cpp md5.cpp guess_mpi.cpp -o main -std=c++11
+
+int main(int argc, char *argv[])
+{
+    MPI_Init(&argc, &argv);
+    int world_rank, world_size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    double time_hash = 0;  // 用于MD5哈希的时间
+    double time_guess = 0; // 哈希和猜测的总时长
+    double time_train = 0; // 模型训练的总时长
+    PriorityQueue q;
+
+    // 只在主进程进行训练并保存模型
+    if (world_rank == 0)
+    {
+        auto start_train = system_clock::now();
+        q.m.train("/guessdata/Rockyou-singleLined-full.txt");
+        q.m.order();
+        auto end_train = system_clock::now();
+        auto duration_train = duration_cast<microseconds>(end_train - start_train);
+        time_train = double(duration_train.count()) * microseconds::period::num / microseconds::period::den;
+        // 保存模型到文件
+        q.m.store("model.dat");
+    }
+
+    // 所有进程同步，确保模型已保存
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    // 所有进程从文件加载模型
+    if (world_rank != 0)
+    {
+        q.m.load("model.dat");
+    }
+
+    q.init();
+    cout << "here" << endl;
+    int curr_num = 0;
+    auto start = system_clock::now();
+    int history = 0;
+    while (!q.priority.empty())
+    {
+        q.PopNext();
+        q.total_guesses = q.guesses.size();
+        if (q.total_guesses - curr_num >= 100000)
+        {
+            cout << "Guesses generated: " << history + q.total_guesses << endl;
+            curr_num = q.total_guesses;
+
+            int generate_n = 10000000;
+            if (history + q.total_guesses > 10000000)
+            {
+                auto end = system_clock::now();
+                auto duration = duration_cast<microseconds>(end - start);
+                time_guess = double(duration.count()) * microseconds::period::num / microseconds::period::den;
+                cout << "Guess time:" << time_guess - time_hash << "seconds" << endl;
+                cout << "Hash time:" << time_hash << "seconds" << endl;
+                cout << "Train time:" << time_train << "seconds" << endl;
+                break;
+            }
+        }
+        if (curr_num > 1000000)
+        {
+            auto start_hash = system_clock::now();
+            bit32 state[4];
+            for (string pw : q.guesses)
+            {
+                MD5Hash(pw, state);
+            }
+            auto end_hash = system_clock::now();
+            auto duration = duration_cast<microseconds>(end_hash - start_hash);
+            time_hash += double(duration.count()) * microseconds::period::num / microseconds::period::den;
+
+            history += curr_num;
+            curr_num = 0;
+            q.guesses.clear();
+        }
+    }
+
+    MPI_Finalize();
+    return 0;
+}
